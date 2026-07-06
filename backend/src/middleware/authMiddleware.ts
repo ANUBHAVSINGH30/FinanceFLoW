79d097cfef8ec8ff0cc2c28826effa7d552b58d0
ANUBHAVSINGH <singhanubhav7795@gmail.com>
2026-07-06 11:55:01 +0530

    authentication complete

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,1 +1,41 @@
-import 
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt.js";
+import { AppError } from "../utils/appError.js";
+import { JwtPayload } from "jsonwebtoken";
+import { findPackageJSON } from "node:module";
+import { tr } from "zod/locales";
+
+export function AuthMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    try{
+        //1. read authentication header
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader){
+            throw new AppError("Authentication headers missing", 401);
+        }
+
+        //2. Check bearer token
+        if(!authHeader.startsWith("Bearer")){
+            throw new AppError("Invalid authorization format", 401);
+        }
+
+        //3. Extract token 
+        const token = authHeader.split(" ")[1];
+
+        //4. verify token 
+        const decoded = verifyToken(token) as JwtPayload & {
+            userId: string
+        };
+
+        //5. Attach userId
+        req.userId =decoded.userId
+
+        next();
+    }catch (error){
+        next(new AppError("Invalid or expired token", 401))
+    };
+};
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { AuthController } from "../controllers/authController.js";
 import { validate } from "../middleware/validateMiddleware.js";
 import { signupSchema, signinSchema } from "../validators/auth.validator.js";
+import { AuthMiddleware } from "../middleware/authMiddleware.js";
 
 const router =Router();
 
 router.post("/signup", validate(signupSchema), AuthController.signup);
 
 router.post("/signin", validate(signinSchema), AuthController.signin);
+
+router.get("/me", AuthMiddleware, AuthController.me)
 
 export default router;

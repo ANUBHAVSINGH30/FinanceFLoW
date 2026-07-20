19736f8350f36f8070b7e897f16187b154e04c9e
ANUBHAVSINGH <singhanubhav7795@gmail.com>
2026-07-20 10:01:01 +0530

    backend complete

---
+++ backend/src/middleware/rateLimitMiddleaware.ts
@@ -0,0 +1,20 @@
+import rateLimit from "express-rate-limit";
+
+//strict for brute force- sensitive routes
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 5,
+    message: { success: false, message: "Too many attempts, please try again after 15 minutes" },
+    standardHeaders: true,
+    legacyHeaders: false
+});
+
+//general for other routes.
+export const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: { success: false, message: "Too many attemp, please try again after 15 min"},
+    standardHeaders: true,
+    legacyHeaders: false
+});
+
